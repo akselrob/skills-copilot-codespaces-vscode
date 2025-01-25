9e40452b96d76785ade0e4c17f18bbe4e8a4d17d
akselrob <akselrob@mcmaster.ca>
2025-01-25 23:30:24 +0000

    Copilot third commit

---
+++ comments.js
@@ -0,0 +1,10 @@
+// Create web server
+// Start web server
+// Handle requests
+// Read data from file
+// Parse data
+// Add new comment
+// Write data to file
+// Respond to request
+// Stop web server
+// Process ends
